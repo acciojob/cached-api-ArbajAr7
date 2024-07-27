fcdd4819008e9e96494173ae89e74243ca09a53e
ArbajAr7 <arbajrmacr786@gmail.com>
2024-07-27 17:00:20 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import DataFetchingComponent from './DataFetchingComponent.js';
+import React, { useState, useEffect, useMemo } from "react";
+import "./../styles/App.css";
+import { fetchData } from "./api";
+import "regenerator-runtime/runtime";
 
-const App = () => {
+function App({ id }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  const cachedData = useMemo(() => {
+    return fetchData(id);
+  }, [id]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setData(null);
+
+    const fetchDataAsync = async () => {
+      const result = await cachedData;
+      setData(result);
+      setIsLoading(false);
+    };
+
+    fetchDataAsync();
+  }, [id, cachedData]);
+
   return (
     <div>
-      <DataFetchingComponent />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <ul>
+            {data.map((data) => (
+              <li key={data.id}>
+                <h4>{data.title}</h4>
+                <p>{data.body}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default App;
--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,9 @@
-import 'regenerator-runtime/runtime'
+
+import "regenerator-runtime/runtime";
 
 export const fetchData = async (id) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const data = await response.json();
-  
-    return data;
-  };
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await response.json();
+
+  return data;
+};
